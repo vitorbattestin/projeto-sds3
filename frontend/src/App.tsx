0fe661eede8a2bf6c066696d1b1db0eeae169be7
Vitor Battestin <vitor.battestin@gmail.com>
2021-05-04 12:24:42 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+  <>
+    <NavBar />
+    <div className="container">
+          <h1 className="text-primary">Olá mundo!</h1>
+          <DataTable />
+      </div>
+      <Footer />
+  </>
   );
 }
 
